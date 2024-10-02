a82fff24faff82f0ce824008d9911d6736932ec7
mobilturka <162546868+03tekno@users.noreply.github.com>
2024-10-02 15:19:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 mkdir /output -p
-apt update
-apt install git grub-pc-bin grub-efi grub-efi-ia32-bin squashfs-tools mtools xorriso rdfind -y
+apt update || true
+apt install git grub-pc-bin grub-efi grub-efi-ia32-bin squashfs-tools mtools xorriso rdfind -y || true
+export FIRMWARE=1
 function build(){
     variant=$1
     suffix=$2
@@ -11,6 +12,7 @@ function build(){
     if [ -f ../$variant.sh ] ; then
         install ../$variant.sh custom
     fi
+    sed -i "s/console=tty31//g" mkiso.sh
     bash -ex mkiso.sh
     mv turkman.iso /output/turkman-$variant$suffix.iso
     echo "##### $(date) #####" > /output/turkman-$variant$suffix.revdep-rebuild
@@ -20,14 +22,4 @@ function build(){
     cd ..
     rm -rf $variant$suffix
 }
-for variant in xfce; do
-    for fw in 0 1 ; do
-        export FIRMWARE=""
-        suffix=""
-        if [[ "$fw" == "1" ]] ; then
-            export FIRMWARE=1
-            suffix="-firmware"
-        fi
-        build $variant $suffix
-    done
-done
+build xfce-enduser
